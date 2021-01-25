4a714262f584b5c39b3e24394d9c5ac65308e803
nhidayahj <hidayah.joha@gmail.com>
2021-01-25 06:05:51 +0000

    arrays-iteraction

---
+++ 05-Arrays/arrays-iteration.js
@@ -0,0 +1,12 @@
+//array iteration 
+let products = ['screwdriver','hammer', 'air fryer', 'hair dryer'];
+
+for (let i = 0; i<products.length; i++) {
+  console.log(products[i]);
+}
+console.log("--------")
+
+//FOR - OF ARRAY -- list travl
+for (let p of products) {
+  console.log(p);
+}
